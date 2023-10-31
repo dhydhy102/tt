fe130786f705bd846c6f59f1ebcfe5c1b7f94ca8
dhy <dhydhy102@gmail.com>
2023-11-01 01:26:02 +0800

    gag

--- src/main/java/tt.java
+++ src/main/java/tt.java
@@ -4,13 +4,14 @@ public class tt {
     public int i ;
 
 
-    public String zvc;
+    public static String zvc = "fasd";
 
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("b");
         System.out.println("b");
         System.out.println(331);
+        System.out.println(zvc);
 
 
         System.out.println(333333);
